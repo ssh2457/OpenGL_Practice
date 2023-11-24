016aaf46745073413cdd966d8f7b31f23962e00d
ssh2457 <ssh245724@gmail.com>
2023-11-24 20:09:50 +0100

    First commit

---
+++ OpenGL_Practice/OpenGL_Practice.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGL_Practice.cpp : Defines the entry point for the application.
+//
+
+#include "OpenGL_Practice.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
